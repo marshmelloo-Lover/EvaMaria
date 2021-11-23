c3ffe42f7edd04f95b3269bb5807e1409402e209
marshmelloo-Lover <94931177+marshmelloo-Lover@users.noreply.github.com>
2021-11-24 01:28:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,24 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    START_TXT = """<b>ʏᴏ.. ʏᴏ.. {}🙋,
+ɪ'ᴍ <a href='https://t.me/Marshmello_Robot'>M̶ꪖ𝘳𝘴ꫝꪑꫀꪶꪶꪮ</a>, ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴀs ᴀᴜᴛᴏ-ғɪʟᴛᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ....
+
+ɪᴛs ᴇᴀsʏ ᴛᴏ ᴜsᴇ ᴍᴇ; ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀs ᴀᴅᴍɪɴ, ᴛʜᴀᴛs ᴀʟʟ, ɪ ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs ᴛʜᴇʀᴇ...🤓
+
+©ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙ𝚢
+<a href='https://t.me/peace_fighter_TG'>»»»»»»»𝙿𝚎𝙰𝚌𝙴-𝙵𝚒𝙶𝚑𝚃𝚎𝚁-𝚃𝙶</a></b>
+
+     """
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """<b>♡︎ ᴍʏ ɴᴀᴍᴇ:{}
+♡︎ ᴄʀᴇᴀᴛᴏʀ: <a href='https://t.me/peace_fighter_TG'>𝙿ᴇ𝙰ᴄ𝙴-𝙵ɪ𝙶ʜ𝚃ᴇ𝚁-ᴛɢ</a>
+♡︎ ᴄʀᴇᴅɪᴛs: ᴇᴠᴇʀʏᴏɴᴇ ɪɴ ᴛʜɪs ᴊᴏᴜʀɴᴇʏ
+♡︎ ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ</a>
+♡︎ ʟᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/'>ᴘʏᴛʜᴏɴᴇ 3</a>♡︎ ᴄʀᴇᴅɪᴛs: ᴇᴠᴇʀʏᴏɴᴇ ɪɴ ᴛʜɪs ᴊᴏᴜʀɴᴇʏ
+♡︎ ᴅᴀᴛᴀ ʙᴀsᴇ: <a href='https://www.mongodb.com/cloud'>ᴍᴀɴɢᴏ ᴅʙ</a>
+♡︎ ʙᴏᴛ sᴇʀᴠᴇʀ: <a href='https://heroku.com/'>ʜᴇʀᴏᴋᴜ</a>
+♡︎ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: <a href='https://t.me/NOKIERUNNOIPPKITTUM'>ᴄʟɪᴄᴋ ᴍᴇ 👈</a>
+♡︎ ʙᴜɪʟᴅ sᴛᴀᴛᴜs: 𝚅7.6 [ ʙᴇᴛᴀ ]</b>"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
